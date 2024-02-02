20a1dadfa2138e739cd57efbce71d35d315f59db
pranshu0604 <135706895+pranshu0604@users.noreply.github.com>
2024-02-02 18:48:06 +0530

    Create day2.cpp

---
+++ day2.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+class user{
+     string name;
+     int age;
+public:
+     user(){
+          cout<<"Enter your name: ";
+          cin>>name;
+          cout<<"Enter your age: ";
+          cin>>age;
+     }
+     greet(){
+          cout<<"Hello "<<name<<". Your age is "<<age<<" years."<<endl;
+};
+
+int main(){
+  user user1();
+  user.greet();
+  return 0;
+}
