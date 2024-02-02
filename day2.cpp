83e124455ff110f657dcfad4881df7990ad0aa2b
pranshu0604 <135706895+pranshu0604@users.noreply.github.com>
2024-02-02 18:54:37 +0530

    Update day2.cpp

--- day2.cpp
+++ day2.cpp
@@ -1,21 +1,21 @@
 #include<iostream>
 using namespace std;
 class user{
-     string name;
-     int age;
+    string name;
+    int age;
 public:
-     user(){     //constructor
-          cout<<"Enter your name: ";
-          cin>>name;
-          cout<<"Enter your age: ";
-          cin>>age;
-     }
-     greet(){
-          cout<<"Hello "<<name<<". Your age is "<<age<<" years."<<endl;
+    user(){     //constructor
+        cout<<"Enter your name: ";
+        cin>>name;
+        cout<<"Enter your age: ";
+        cin>>age;
+    }
+    void greet(){
+        cout<<"Hello "<<name<<". Your age is "<<age<<" years."<<endl;}
 };
 
 int main(){
-  user user1();
-  user.greet();
+  user user1;
+  user1.greet();
   return 0;
 }
