9b70c413eec3d4367556f18f8db8d7296f05d417
pranshu0604 <135706895+pranshu0604@users.noreply.github.com>
2024-02-22 21:22:06 +0530

    Create day22.cpp

---
+++ day22.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int fibonacci(int n) {
+    if (n <= 1) {
+        return n;
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main() {
+    int n;
+    std::cout << "Enter the value of n: ";
+    std::cin >> n;
+    std::cout << "The " << n << "th Fibonacci number is: " << fibonacci(n) << std::endl;
+    return 0;
+}
